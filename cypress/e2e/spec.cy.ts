d4033e5b488a958ec570fe80f6a0e2fadab37654
sakib-celloscope <sakib.celloscope@gmail.com>
2024-10-14 15:10:37 +0600

    added missing employees

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -4,6 +4,10 @@ describe('Login Page Test', () => {
     cy.fixture('employee').then((data_set) => {
       const employee_data = data_set.employee_data;
       cy.log(employee_data.length)
+      let employee_id_arr = employee_data.map((item: any) => item.employee_id);
+      cy.log(employee_id_arr);
+      console.log(employee_id_arr);
+      
     });
   });
 });
